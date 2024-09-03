2440a38d54bd70acfa898baf9f9168a2a7e68467
Alpha <81774909+alpha-Issagha@users.noreply.github.com>
2024-09-04 00:37:31 +0200

    Initial commit

---
+++ src/main/java/com/example/demo/student/StudentConfig.java
@@ -0,0 +1,37 @@
+package com.example.demo.student;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+
+import java.time.LocalDate;
+import java.util.List;
+
+
+import static java.time.Month.JANUARY;
+
+@Configuration
+public class StudentConfig {
+    @Bean
+    CommandLineRunner commandLineRunner(
+            StudentRepository repository){
+        return args -> {
+           Student mariam =  new Student(
+                    1L,
+                    "Mariam",
+                    "mariam.jamal@gmail.com",
+                    LocalDate.of(2000, JANUARY,5)
+           );
+            Student alpha = new Student(
+                    "Alpha",
+                    "alpha@gmail.com",
+                    LocalDate.of(1997, JANUARY,18)
+            );
+            repository.saveAll(
+                    List.of (mariam,alpha)
+            );
+        };
+   }
+}
+
